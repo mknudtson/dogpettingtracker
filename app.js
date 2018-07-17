e7990a55396df631650f80ed6a30e1e006ddc361
Melissa Knudtson <melissa.erin.knudtson@gmail.com>
2018-07-16 22:52:07 -0400

    improve styles, improve structure, restore default values, improve button function

--- app.js
+++ app.js
@@ -7,31 +7,53 @@ $(document).ready(function(){
     // store each input as a var
     // HOMEWORK ADD AND DELETE
     
-    $("#trackbutton").on("click", function(event) {
+    $("#trackbutton").on("click", function(event) { // on is the new bind
         event.preventDefault();     
 
             let issueInput = $('#field-issue').val();
             let issueSeverity = $('#field-severity').val();
-            let issueAssigned = $('#field-assigned').val()  ;
+            let issueAssigned = $('#field-assigned').val();
         
-            $('#display-issues').append(`<ul class="issue"></ul>`);
-        
-            $('.issue:last').append(`<form class="form-issue">`).css('border', '2px solid red').css('border-radius', '2%');
-        
-            $('.form-issue:last').append(`<button class="btn" type="submit">open</button>`);
-            $('.form-issue:last').append(`<button class="btn" type="submit">Close</button>`);
-            $('.form-issue:last').append(`<button class="btn" type="submit">Delete</button>`);  
+            // add div, although i'm not sure it's needed as it's adding to a div
+            // note #display-issues is a div already in the body of the html file
+            $('#display-issues').append(`<div class="issues"></ul>`).css('background-color', 'yellow');
             
-            $('.issue:last').append("<li>" + `Issue: ${issueInput}`+ "</li>")
-            $('.issue:last').append("<li>" + `Severity: ${issueSeverity}`+ "</li>")
-            $('.issue:last').append("<li>" + `Assigned to: ${issueAssigned}`+ "</li>")
+            // to div, append ul; each group of issue chars is a ul and is styled
+            $('.issues:last').append(`<ul class="ul-issue">`).css('border', '2px solid #DAA520').css('border-radius', '2%');
             
-            // __ fix default input, and clear input following submission        
-            // __ need to use a different identifier each time - [0]?
-            // is it possible to have an array of uls?
-            // jquery each?
+            // to ul, append, as an li, an open/closed button
+            $('.ul-issue:last').append("<li class='li-inline'>" + `<button class="btn btn-append btn-status" type="submit">open</button>` + "</li>");  
+            
+            // to form, append lis, one per issue char
+            $('.ul-issue:last').append("<li>" + `Dog: ${issueInput}`+ "</li>");
+            $('.ul-issue:last').append("<li>" + `Urgency: ${issueSeverity}`+ "</li>");
+            $('.ul-issue:last').append("<li>" + `Petter: ${issueAssigned}`+ "</li>");
+            
+            // to ul, append, as lis, a close button and a delete button, and style them as inline-block
+            $('.ul-issue:last').append("<li class='li-inline'>" + `<button class="btn btn-append btn-close" type="submit">Close</button>` + "</li>").css('display', 'inline-block');
+            $('.ul-issue:last').append("<li class='li-inline'>" + `<button class="btn btn-append btn-delete" type="submit">Delete</button>` + "</li>").css('display', 'inline-block');
+            
+            // when done, restore default vals in input fields
+            issueInput = $('#field-issue').val("");
+            issueSeverity = $('#field-severity').val("Low");
+            issueAssigned = $('#field-assigned').val("");
+            
+            // 1st, config delete button to delete uls with .issues
+            // 2nd, config it to delete THE RIGHT ul with arrays...
+            $('.btn-delete').on('click', function(){
+                $(".issues").hide();
+            });
+
+            // _x_ when btn-submit pressed, all input fields should be restored to default input
+            // __ when btn-delete pressed, issue should disappear
+            // __ when btn-close  pressed, text of btn-status should be toggled from open to closed
             
     });
     
+            if ($('button').hasClass("blue")) {
+                $('button').attr("value", "closed");
+            } else {
+                $('button').attr("value", "open");
+            } 
     
 });
